075a67a26eceb447088e284f8c7d7525b13e2644
Полина <Shumilina246@gmail.com>
2022-05-23 15:30:20 +0300

    create: group section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -57,7 +57,15 @@ export const Home = () => {
             <button className={s.bid_button}>отправить</button>
           </div>
         </section>
-        <section>Group</section>
+        <section className={s.group}>
+          <h3 className={s.group_title}>О группе</h3>
+          <p className={s.group_text}>
+            Twenty One Pilots — американский дуэт из Колумбуса, штат Огайо.
+            Группа образовалась в 2009 году и на данный момент состоит из
+            Тайлера Джозефа и Джоша Дана. Коллектив самостоятельно выпустил два
+            альбома: Twenty One Pilots в 2009 и Regional at Best в 2011.
+          </p>
+        </section>
       </div>
     </>
   );
