ff6d56f05932697d9ecc2d69f05872cbf5f8422c
Полина <Shumilina246@gmail.com>
2022-05-23 12:36:55 +0300

    add: cards slider

--- src/pages/Home/components/Tickets/Tickets.tsx
+++ src/pages/Home/components/Tickets/Tickets.tsx
@@ -1,5 +1,6 @@
 import { Card } from "./Card";
 import s from "./Tickets.module.scss";
+import Slider from "react-slick";
 
 export type User = {
   name: string;
@@ -8,11 +9,27 @@ export type User = {
 
 export const Tickets = () => {
   const users: User[] = [
-    { name: "Иванов Cемен", city: "Санкт-Петербург" },
-    { name: "Иванов Cемен", city: "Санкт-Петербург" },
-    { name: "Иванов Cемен", city: "Санкт-Петербург" },
-    { name: "Иванов Cемен", city: "Санкт-Петербург" },
+    { name: "Иванов Cемен 1", city: "Санкт-Петербург" },
+    { name: "Иванов Cемен 2", city: "Санкт-Петербург" },
+    { name: "Иванов Cемен 3", city: "Санкт-Петербург" },
+    { name: "Иванов Cемен 4", city: "Санкт-Петербург" },
+    { name: "Иванов Cемен 5", city: "Санкт-Петербург" },
+    { name: "Иванов Cемен 6", city: "Санкт-Петербург" },
+    { name: "Иванов Cемен 7", city: "Санкт-Петербург" },
+    { name: "Иванов Cемен 8", city: "Санкт-Петербург" },
+    { name: "Иванов Cемен 9", city: "Санкт-Петербург" },
+    { name: "Иванов Cемен 10", city: "Санкт-Петербург" },
   ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    arrows: false,
+    focusOnSelect:true,
+    speed: 700,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  };
 
   return (
     <section className={s.tickets}>
@@ -22,11 +39,12 @@ export const Tickets = () => {
           932/ <span>1000</span>
         </span>
       </div>
-      <div className={s.cards}>
+
+      <Slider className={s.cards} {...settings}>
         {users.map((user) => (
           <Card name={user.name} city={user.city} />
         ))}
-      </div>
+      </Slider>
     </section>
   );
 };
