7a538cbefde2daee1abbe4a756230297f25f99dc
Полина <Shumilina246@gmail.com>
2022-05-23 16:03:27 +0300

    create: userPage user section

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -1,5 +1,20 @@
 import s from "./UserPage.module.scss";
 
 export const UserPage = () => {
-  return <div>UserPage</div>;
+  return (
+    <>
+      <div className="container">
+        <section className={s.user}>
+          <h2 className={s.title}>Иванов Семен</h2>
+          <div className={s.info}>
+            <span>Санкт-Петербург</span>
+            <span>ivanov@mail.com</span>
+            <span>+7 (821) 311-21-32</span>
+            <button className={s.message}>Написать сообщение</button>
+            <button className={s.offer}>Предложить сходить на концерт</button>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 };
